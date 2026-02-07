45caed02f2a996a65b91ecb128f08f8c05888355
Anjani109 <anjanimvn@gmail.com>
2026-02-07 18:30:49 +0530

    Add project files.

---
+++ Models/Medicine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PharmacyInventoryWebApp.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Medicine
+    {
+        public int MedicineId { get; set; }
+
+        [Required]
+        public string MedicineName { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+        public decimal UnitPrice { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+
+}
---
+++ Models/PharmacyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyInventoryWebApp.Models;
+
+namespace PharmacyInventoryWebApp.Models
+{
+    public class PharmacyContext : DbContext
+    {
+        public PharmacyContext(DbContextOptions<PharmacyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Medicine> Medicines { get; set; }
+    }
+}
+
